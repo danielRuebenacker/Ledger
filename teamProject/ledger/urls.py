5f02e74289dcab10c9ee6e98966bcfb8a6353dbe
Daniel R <2902294r@student.gla.ac.uk>
2026-02-27 20:11:36 +0000

    Add templates
    Made templates directory with base template (start of what will be menu bar).

--- teamProject/ledger/urls.py
+++ teamProject/ledger/urls.py
@@ -5,4 +5,7 @@ app_name = 'ledger'
 
 urlpatterns = [
         path('', views.index, name='index'),
+        path('myhabits', views.myhabits, name='myhabits'),
+        path('leaderboards', views.leaderboards, name='leaderboards'),
+        path('social', views.social, name='social'),
 ]
--- teamProject/ledger/views.py
+++ teamProject/ledger/views.py
@@ -4,4 +4,21 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return HttpResponse('index!')
+    context_dict = {}
+
+    return render(request, 'ledger/index.html', context=context_dict)
+
+def myhabits(request):
+    context_dict = {}
+
+    return render(request, 'ledger/myhabits.html', context=context_dict)
+
+def leaderboards(request):
+    context_dict = {}
+
+    return render(request, 'ledger/myhabits.html', context=context_dict)
+
+def social(request):
+    context_dict = {}
+
+    return render(request, 'ledger/social.html', context=context_dict)
