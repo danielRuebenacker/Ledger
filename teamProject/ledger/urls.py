11803f46708cfc216fe10dc642a6370d0cedbe8a
Yitai Lyu <3070833l@student.gla.ac.uk>
2026-03-11 02:01:21 +0000

    friends page layout

--- teamProject/ledger/urls.py
+++ teamProject/ledger/urls.py
@@ -4,8 +4,9 @@ from ledger import views
 app_name = 'ledger'
 
 urlpatterns = [
-        path('', views.index, name='index'),
-        path('myhabits', views.myhabits, name='myhabits'),
-        path('leaderboards', views.leaderboards, name='leaderboards'),
-        path('social', views.social, name='social'),
+    path('', views.index, name='index'),
+    path('myhabits', views.myhabits, name='myhabits'),
+    path('leaderboards', views.leaderboards, name='leaderboards'),
+    path('social', views.social, name='social'),
+    path('friends', views.friends, name='friends'),
 ]
--- teamProject/ledger/views.py
+++ teamProject/ledger/views.py
@@ -3,23 +3,33 @@ from django.shortcuts import render
 
 # Create your views here.
 
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+
 def index(request):
     context_dict = {}
-
     return render(request, 'ledger/index.html', context=context_dict)
 
 def myhabits(request):
     context_dict = {}
-
     return render(request, 'ledger/myhabits.html', context=context_dict)
 
 def leaderboards(request):
     context_dict = {}
-
-    return render(request, 'ledger/myhabits.html', context=context_dict)
+    return render(request, 'ledger/leaderboards.html', context=context_dict)
 
 def social(request):
     context_dict = {}
-
     return render(request, 'ledger/social.html', context=context_dict)
 
+def friends(request):
+    context_dict = {
+        'friends_list': [
+            {'name': 'Alice', 'streak': 12, 'points': 340},
+            {'name': 'Ben', 'streak': 8, 'points': 210},
+            {'name': 'Chloe', 'streak': 15, 'points': 410},
+        ]
+    }
+    return render(request, 'ledger/friends.html', context=context_dict)
