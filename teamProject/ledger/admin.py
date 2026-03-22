389635a2af4235d3259deaead3e1ea0bf95bf60b
Daniel R <2902294r@student.gla.ac.uk>
2026-03-22 15:59:46 +0000

    add models to admin interface for testing

--- teamProject/ledger/admin.py
+++ teamProject/ledger/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from ledger.models import UserProfile
+from ledger.models import UserProfile, Habit, HabitTracker
 
 # Register your models here.
 admin.site.register(UserProfile)
+admin.site.register(Habit)
+admin.site.register(HabitTracker)
