a3531effae74ccb83dbb0c1332ec1eb1b931b49a
Daniel R <2902294r@student.gla.ac.uk>
2026-03-22 15:04:45 +0000

    create habits in view

--- teamProject/ledger/views.py
+++ teamProject/ledger/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-# from ledger.utils import habit_tracker
+from ledger.utils import habit_utils
 from ledger.forms import HabitTrackerForm
+from ledger.models import Habit
 
 # Create your views here.
 
@@ -12,21 +13,26 @@ def index(request):
 
 def myhabits(request):
     context_dict = {}
-    
+    user_profile = request.user.userprofile
     # if habit tracker is not created present form view
-    if True:
+    habit_tracker = habit_utils.get_current_month_habit_tracker(user_profile)
+    
+    if not habit_tracker:
+        # create one
         if request.method == 'POST':
             form = HabitTrackerForm(request.POST)
 
             if form.is_valid():
-                dos = form.cleaned_data.get('dos')
-                donts = form.cleaned_data.get('donts')
-                easy_wins = form.cleaned_data.get('easy_wins')
+                # ignore empty strings
+                dos_strings = [h for h in form.cleaned_data.get('dos') if h]
+                donts_strings = [h for h in form.cleaned_data.get('donts') if h]
+                easy_wins_strings = [h for h in form.cleaned_data.get('easy_wins') if h] 
 
                 # make these into habits
-                print(dos)
-                print(donts)
-                print(easy_wins)
+                dos = habit_utils.get_or_create_habits_from_list(dos_strings, Habit.TYPE_DO)
+                donts = habit_utils.get_or_create_habits_from_list(donts_strings, Habit.TYPE_DONT)
+                easy_wins = habit_utils.get_or_create_habits_from_list(easy_wins_strings, Habit.TYPE_EASY_WIN)
+
         else:
             form = HabitTrackerForm()
 
