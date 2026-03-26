163bae4d469ab2c5e531d0745a5eaafe61c09f1a
William Chen <2999784C@student.gla.ac.uk>
2026-03-26 17:41:41 +0000

    Leaderboard + url fixes and customregistration fixed

--- teamProject/ledger/admin.py
+++ teamProject/ledger/admin.py
@@ -1,13 +1,24 @@
 from django.contrib import admin
-from ledger.models import UserProfile, Habit, Nudge, Friendship, HabitTracker, Day, BoolHabitEntry, JournalEntry, FriendRequest
+from ledger.models import (
+    UserProfile, Habit, Nudge, Friendship, HabitTracker, 
+    Day, BoolHabitEntry, JournalEntry, FriendRequest
+)
 
-# Register your models here.
+# Use the @register decorator to link the model to the custom Admin class
+@admin.register(HabitTracker)
+class HabitTrackerAdmin(admin.ModelAdmin):
+    # This defines the columns that appear in the admin table
+    list_display = ('user', 'month', 'streak')
+    
+    # This adds a sidebar to filter data by month or streak value
+    list_filter = ('month', 'streak')
+
+# Register the remaining models normally
 admin.site.register(BoolHabitEntry)
 admin.site.register(Day)
 admin.site.register(FriendRequest)
 admin.site.register(Friendship)
 admin.site.register(Habit)
-admin.site.register(HabitTracker)
 admin.site.register(JournalEntry)
 admin.site.register(Nudge)
 admin.site.register(UserProfile)
--- teamProject/ledger/urls.py
+++ teamProject/ledger/urls.py
@@ -1,14 +1,25 @@
-from django.urls import path
+from django.urls import path, include, reverse_lazy
 from ledger import views
+from registration.backends.simple.views import RegistrationView
+from ledger.forms import CustomRegistrationForm
 
 app_name = 'ledger'
 
 urlpatterns = [
+        path(
+        'accounts/register/',
+        RegistrationView.as_view(
+            form_class=CustomRegistrationForm,
+            success_url=reverse_lazy('ledger:index')
+                ),
+                name='registration_register',
+        ),
+        path('accounts/', include('registration.backends.simple.urls')),
         path('', views.index, name='index'),
-        path('myhabits', views.myhabits, name='myhabits'),
-        path('leaderboards', views.leaderboards, name='leaderboards'),
-        path('social', views.social, name='social'),
-        path('profile', views.profile, name='profile'),
+        path('myhabits/', views.myhabits, name='myhabits'),
+        path('leaderboards/', views.leaderboards, name='leaderboards'),
+        path('social/', views.social, name='social'),
+        path('profile/', views.profile, name='profile'),
         path('settings/',views.settings, name='settings'),
         path('profile/<str:username>/', views.profile, name='profile_user'),
         path('api/notifications/get_notifs', views.get_notifications, name='notifications'),
