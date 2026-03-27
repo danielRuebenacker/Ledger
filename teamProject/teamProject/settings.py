05192877d9457cd1417aa1a66b28118e03d745bb
danielRuebenacker <88078970+danielRuebenacker@users.noreply.github.com>
2026-03-27 11:21:30 +0000

    Fix requirements (#56)
    * correctly type requirements.txt
    
    * delete wrong reqs file + add allowed hosts in settings
    
    ---------
    
    Co-authored-by: Daniel R <2902294r@student.gla.ac.uk>

--- teamProject/teamProject/settings.py
+++ teamProject/teamProject/settings.py
@@ -49,7 +49,7 @@ SECRET_KEY = 'i68#)6x_*w^)+mw1ojhs%$cx-y%uk=1avd9qr6yolwzt%hd(um'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['williamo.pythonanywhere.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
